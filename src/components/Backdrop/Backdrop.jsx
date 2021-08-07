c0704b603ab69eecf77990ddbb8cb0d70d6dfc9d
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-07 18:21:47 +0300

    Backdrop component created in /src/components directory

---
+++ src/components/Backdrop/Backdrop.jsx
@@ -0,0 +1,25 @@
+import PropTypes from "prop-types";
+
+import classes from "./Backdrop.module.scss";
+
+/**
+ * this component adds a semi-transparent layer, which is dismissible upon click, to screen
+ */
+const Backdrop = ({ isBackdropActivated, onDismiss }) => {
+  return (
+    isBackdropActivated && (
+      <button className={classes.Backdrop} onClick={onDismiss}></button>
+    )
+  );
+};
+
+Backdrop.propTypes = {
+  isBackdropActivated: PropTypes.bool,
+  onDismiss: PropTypes.func.isRequired,
+};
+
+Backdrop.defaultProps = {
+  isBackdropActivated: false,
+};
+
+export default Backdrop;
