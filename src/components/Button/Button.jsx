4faa8b245e63641cb46e9a851f41e63441315349
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-08 17:14:52 +0300

    content of <Button> component updated in Button.jsx file

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,10 +2,18 @@ import PropTypes from "prop-types";
 
 import classes from "./Button.module.scss";
 
-const Button = ({ children, onClicked, type }) => {
+const Button = ({
+  children,
+  isBlockButton,
+  onClicked,
+  overrideClassName,
+  type,
+}) => {
   return (
     <button
-      className={`${classes.Button} ${classes[type]}`}
+      className={`${classes.Button} ${
+        isBlockButton ? classes.BlockButton : classes.InlineButton
+      } ${classes[type]} ${overrideClassName}`}
       onClick={onClicked}
     >
       {children}
@@ -15,11 +23,15 @@ const Button = ({ children, onClicked, type }) => {
 
 Button.propTypes = {
   children: PropTypes.node.isRequired,
+  isBlockButton: PropTypes.bool,
   onClicked: PropTypes.func.isRequired,
+  overrideClassName: PropTypes.string,
   type: PropTypes.string,
 };
 
 Button.defaultProps = {
+  isBlockButton: false,
+  overrideClassName: "",
   type: "Primary",
 };
 
