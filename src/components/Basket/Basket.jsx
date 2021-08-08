a6fe41b96cfc1cf4c70e5d6dedf0f5d86b6ed572
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-08 19:08:14 +0300

    dynamic rendering of products in <Basket> component added in Basket.jsx file

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -1,17 +1,31 @@
+import { useSelector } from "react-redux";
 import classes from "./Basket.module.scss";
 
 import ProductInBasket from "../ProductInBasket";
 import Text from "../Text";
 
 const Basket = () => {
+  const cart = useSelector((state) => state.cartSlice.cart);
+  const totalPrice = useSelector((state) => state.cartSlice.totalPrice);
+
+  // TODO: add a condition for when cart.length = 0. display an informative text to user that s/he has no item(s) in basket
   return (
     <div className={classes.Basket}>
-      <ProductInBasket />
-      <ProductInBasket />
-      <ProductInBasket />
-      <ProductInBasket />
+      {cart.length === 0 ? (
+        <span>No products available</span>
+      ) : (
+        cart.map((product) => (
+          <ProductInBasket
+            key={product.id}
+            id={product.id}
+            name={product.name}
+            price={product.price}
+            quantity={product.quantity}
+          />
+        ))
+      )}
       <div className={classes.BasketPrice}>
-        <Text fontWeight="FontWeight600" text="₺ 39.97" />
+        <Text fontWeight="FontWeight600" text={`₺ ${totalPrice.toFixed(2)}`} />
       </div>
     </div>
   );
