9ff09005277f90603cc83997144f9316b17202e3
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-08 19:31:47 +0300

    returned JSX element wrapped with <section> element in Basket.jsx file

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -10,24 +10,29 @@ const Basket = () => {
 
   // TODO: add a condition for when cart.length = 0. display an informative text to user that s/he has no item(s) in basket
   return (
-    <div className={classes.Basket}>
-      {cart.length === 0 ? (
-        <span>No products available</span>
-      ) : (
-        cart.map((product) => (
-          <ProductInBasket
-            key={product.id}
-            id={product.id}
-            name={product.name}
-            price={product.price}
-            quantity={product.quantity}
+    <section className={classes.RightSection}>
+      <div className={classes.Basket}>
+        {cart.length === 0 ? (
+          <span>No products available</span>
+        ) : (
+          cart.map((product) => (
+            <ProductInBasket
+              key={product.id}
+              id={product.id}
+              name={product.name}
+              price={product.price}
+              quantity={product.quantity}
+            />
+          ))
+        )}
+        <div className={classes.BasketPrice}>
+          <Text
+            fontWeight="FontWeight600"
+            text={`₺ ${totalPrice.toFixed(2)}`}
           />
-        ))
-      )}
-      <div className={classes.BasketPrice}>
-        <Text fontWeight="FontWeight600" text={`₺ ${totalPrice.toFixed(2)}`} />
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
