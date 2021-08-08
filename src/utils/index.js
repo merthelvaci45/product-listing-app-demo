99aef7dcef1b57b22c95b4d996385b6fc81d2685
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-08 17:09:55 +0300

    /srtc/utils/index.js file content updated to include new re-exports

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,8 +1,18 @@
 import {
-  API_BASE_URL,
+  COMPANIES_API_BASE_URL,
+  ITEMS_API_BASE_URL,
   API_METHODS,
   PAGE_ROUTES,
   SORT_OPTIONS,
 } from "./constants";
 
-export { API_BASE_URL, API_METHODS, PAGE_ROUTES, SORT_OPTIONS };
+import findNumberOfOccurencesOfItemsInArray from "./findNumberOfOccurencesOfItemsInArray";
+
+export {
+  API_METHODS,
+  COMPANIES_API_BASE_URL,
+  ITEMS_API_BASE_URL,
+  PAGE_ROUTES,
+  SORT_OPTIONS,
+  findNumberOfOccurencesOfItemsInArray,
+};
