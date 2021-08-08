103d40ae852bde6d47ae02fdef5c52ad05f52e1c
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-08 17:12:03 +0300

    content of "useAPI" hook updated in useAPI.js file

--- src/hooks/useAPI.js
+++ src/hooks/useAPI.js
@@ -1,52 +1,42 @@
 import { useEffect, useState } from "react";
 
 import { axiosInstance } from "../config";
-import { API_BASE_URL, API_METHODS } from "../utils";
+import { API_METHODS } from "../utils";
 
 /**
- * this hook is responsible for making an API request using its 2 named parameters "queryString" and "method",
- * and two positional arguments "headers" and "body". note that "headers" and "body" parameters are here just for
- * reference to denote that this hook can also be used for other API method invocations (POST, PUT etc.) and not
- * just for GET requests.
- * "queryString" and "method" paramters have their respective default values in case they are not provided as
- * arguments when the hook is invoked.
- * "method" parameter is defined to have an "enum" like value provided by "API_METHODS" object. for further details,
+ * this hook is responsible for making an API request using its 4 optional named parameters "queryPath", "method",
+ * "headers" and "body". All of them are initialized to their respective default values. Note that "body" parameter
+ * should be provided for API requests other than GET method as described in the documentation of "axios" package.
+ * Since fetched data will be in JSON format, "Content-Type" is set to default as "application/json". If API request
+ * will be successful, axios instance returns an object with "data" property in which all API data is held. For this
+ * reason, "data" property is pulled out from the request result.
+ * "method" parameter is defined to have an "enum" like value provided by "API_METHODS" object. For further details,
  * please refer to "../utils/constants.js".
  */
 const useAPI = ({
-  queryString = "",
+  queryPath = "",
   method = API_METHODS.GET,
-  headers = {},
+  headers = { "Content-Type": "application/json" },
   body = {},
 }) => {
   const [apiData, setApiData] = useState(); // state to hold returned API data from backend in case of successful request
-  const [isLoading, setIsLoading] = useState(false); // state to be set while API request is in progress and to display user a loading indicator
+  const [isLoading, setIsLoading] = useState(true); // state to be set while API request is in progress and to display user a loading indicator
   const [isError, setIsError] = useState(false); // state to be set in case of any error while making request to API
 
   useEffect(() => {
     // this handler function is responsible for making the request to API
     const fetchFromAPIHandler = async () => {
       try {
-        // at the start of request, reset "isLoading" and "isError" states to initial values
-        setIsLoading(true);
+        // at the start of a request, reset "isError" state to initial value
         setIsError(false);
 
-        switch (method) {
-          case API_METHODS.GET:
-            const { data } = await axiosInstance[method](
-              `${API_BASE_URL}/${queryString}`
-            ); // make API request
-            setApiData(() => data); // update "apiData" state
-            break;
-
-          case API_METHODS.POST:
-          case API_METHODS.PUT:
-            // TODO: Put a sample POST/PUT methods logic in here
-            break;
-
-          default:
-            return;
-        }
+        const { data } = await axiosInstance({
+          method,
+          url: queryPath,
+          headers,
+          data: method !== API_METHODS.GET ? body : null,
+        }); // make API request
+        setApiData(() => data); // update "apiData" state
       } catch (error) {
         setIsError(true); // set "isError" state to true in case of error
         console.log(error);
@@ -56,7 +46,7 @@ const useAPI = ({
     };
 
     fetchFromAPIHandler(); // invoke the handler function
-  }, [queryString, method]);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return [apiData, isLoading, isError];
 };
