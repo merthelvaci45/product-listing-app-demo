8c53fa51734f5ea364b392bdf5dc46aacccef13c
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-08 21:31:09 +0300

    products sorting reducer function and logic added in products-slice.js file

--- src/store/slices/products-slice.js
+++ src/store/slices/products-slice.js
@@ -1,6 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = { products: [] };
+import { initialSortingRadioButtonsState, SORT_OPTIONS_IDS } from "../../utils";
+
+const initialState = {
+  products: [],
+  sortingOptions: initialSortingRadioButtonsState,
+};
 
 const productsSlice = createSlice({
   name: "products",
@@ -8,6 +13,40 @@ const productsSlice = createSlice({
   reducers: {
     populateProducts(state, action) {
       state.products = [...action.payload.products];
+    },
+    sortProductsBy(state, action) {
+      const { selectedSortingOption } = action.payload;
+      state.sortingOptions = {
+        ...initialSortingRadioButtonsState,
+        [selectedSortingOption]: true,
+      };
+
+      let sortedProducts = [...state.products];
+      switch (selectedSortingOption) {
+        case SORT_OPTIONS_IDS.ASCENDING_PRICE:
+          sortedProducts.sort((first, second) => first.price - second.price);
+          break;
+
+        case SORT_OPTIONS_IDS.DESCENDING_PRICE:
+          sortedProducts.sort((first, second) => second.price - first.price);
+          break;
+
+        case SORT_OPTIONS_IDS.TO_OLDEST:
+          sortedProducts.sort((first, second) => first.added - second.added);
+          break;
+
+        case SORT_OPTIONS_IDS.TO_NEWEST:
+          sortedProducts.sort((first, second) => second.added - first.added);
+          break;
+
+        default:
+          break;
+      }
+      state.products = sortedProducts;
+      state.products.forEach((product, index) => {
+        if (index > 5) return;
+        console.log(`name: ${product.name}\nprice: ${product.price}\n\n`);
+      });
     },
   },
 });
