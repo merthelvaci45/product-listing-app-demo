797aafcae72aaccbf5f33b24135d9a584b3be798
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-08 20:49:01 +0300

    unused useRadioButton hook removed from repository

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,11 +1,5 @@
 import useAPI from "./useAPI";
 import useManufacturerCountForItemType from "./useManufacturerCountForItemType";
-import useRadioButton from "./useRadioButton";
 import useTagCountForItemType from "./useTagCountForItemType";
 
-export {
-  useAPI,
-  useManufacturerCountForItemType,
-  useRadioButton,
-  useTagCountForItemType,
-};
+export { useAPI, useManufacturerCountForItemType, useTagCountForItemType };
