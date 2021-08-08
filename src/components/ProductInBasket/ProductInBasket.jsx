6f075657e863c305e25a34b26ffbd0174baad3a2
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-08 19:10:49 +0300

    redux store product data inserted in ProductInBasket.jsx file

--- src/components/ProductInBasket/ProductInBasket.jsx
+++ src/components/ProductInBasket/ProductInBasket.jsx
@@ -1,19 +1,32 @@
+import { useDispatch } from "react-redux";
+import PropTypes from "prop-types";
+
 import classes from "./ProductInBasket.module.scss";
+
+import { cartActions } from "../../store/slices";
 
 import FlatButton from "../FlatButton";
 import Text from "../Text";
 
-const ProductInBasket = () => {
+const ProductInBasket = ({ id, name, price, quantity }) => {
+  const dispatch = useDispatch();
+
+  const removeProductFromCartHandler = () => {
+    dispatch(cartActions.removeItemFromCart({ id }));
+  };
+
+  const addProductToCartHandler = () => {
+    dispatch(cartActions.addItemToCart({ id, name, price }));
+  };
+
   return (
     <div className={classes.ProductInBasket}>
       <div className={classes.ProductNameAndPrice}>
-        <Text color="TextPrimary" text="Product Name" />
-        <Text fontWeight="FontWeight600" text="₺ 14.99">
-          Price
-        </Text>
+        <Text color="TextPrimary" text={name} />
+        <Text fontWeight="FontWeight600" text={`₺ ${price}`} />
       </div>
       <div className={classes.ProductQuantity}>
-        <FlatButton onPressed={() => {}}>
+        <FlatButton onPressed={removeProductFromCartHandler}>
           <i className="fas fa-minus"></i>
         </FlatButton>
         <div>
@@ -21,15 +34,22 @@ const ProductInBasket = () => {
             color="White"
             fontSize="FontSize15"
             fontWeight="FontWeight700"
-            text="1"
+            text={quantity.toString()}
           />
         </div>
-        <FlatButton onPressed={() => {}}>
+        <FlatButton onPressed={addProductToCartHandler}>
           <i className="fas fa-plus"></i>
         </FlatButton>
       </div>
     </div>
   );
+};
+
+ProductInBasket.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  quantity: PropTypes.number.isRequired,
 };
 
 export default ProductInBasket;
