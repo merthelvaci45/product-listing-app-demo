752bfa22da31055ff58b486a2c8c6c00f7579b72
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-08 17:18:03 +0300

    actions and reducers for redux slices re-exported in /src/store/slices/index.js file

---
+++ src/store/slices/index.js
@@ -0,0 +1,4 @@
+import { cartReducer, cartActions } from "./cart-slice";
+import { productsReducer, productsActions } from "./products-slice";
+
+export { cartReducer, cartActions, productsReducer, productsActions };
