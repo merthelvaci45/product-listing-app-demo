56f1b1e704236bf1b6ceea9f363e43ed4798ce53
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-09 13:07:19 +0300

    pagination actions and rendering data added in Pagination.jsx file

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,32 +1,69 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import PropTypes from "prop-types";
+
 import classes from "./Pagination.module.scss";
+
+import { paginationActions } from "../../store/slices";
 
 import FlatButton from "../../components/FlatButton";
 import Text from "../../components/Text";
 
-const Pagination = () => {
+const Pagination = ({ totalNumberOfPages }) => {
+  const dispatch = useDispatch();
+  const { pageNumber, pageNumbers } = useSelector(
+    (state) => state.paginationSlice
+  );
+
+  const incrementPageNumberHandler = () => {
+    dispatch(paginationActions.incrementPageNumber());
+  };
+
+  const decrementPageNumberHandler = () => {
+    dispatch(paginationActions.decrementPageNumber());
+  };
+
+  const setPageNumberHandler = ({ currentTarget }) => {
+    const { id } = currentTarget;
+    dispatch(paginationActions.setPageNumberTo(parseInt(id)));
+  };
+
+  useEffect(() => {
+    dispatch(paginationActions.setTotalPageNumbers(totalNumberOfPages));
+  }, [dispatch, totalNumberOfPages]);
+
   return (
     <div className={classes.Pagination}>
-      <FlatButton onPressed={() => {}}>
+      <FlatButton onPressed={decrementPageNumberHandler}>
         <i className="fas fa-arrow-left"></i>
         <Text fontWeight="FontWeight600" text="Prev" />
       </FlatButton>
       <div className={classes.PageNumbers}>
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
-        <span>4</span>
-        <span>...</span>
-        <span>5</span>
-        <span>6</span>
-        <span>7</span>
-        <span>8</span>
+        {pageNumbers.map((number) => {
+          //if (index === 4) return <span key={number}>...</span>;
+          return (
+            <FlatButton
+              key={number}
+              id={number.toString()}
+              isBorderedStyle={pageNumber === number}
+              isNotPrimaryColor
+              onPressed={setPageNumberHandler}
+            >
+              {number}
+            </FlatButton>
+          );
+        })}
       </div>
-      <FlatButton onPressed={() => {}}>
+      <FlatButton onPressed={incrementPageNumberHandler}>
         <Text fontWeight="FontWeight600" text="Next" />
         <i className="fas fa-arrow-right"></i>
       </FlatButton>
     </div>
   );
+};
+
+Pagination.propTypes = {
+  totalNumberOfPages: PropTypes.number.isRequired,
 };
 
 export default Pagination;
