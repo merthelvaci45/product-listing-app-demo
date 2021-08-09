0a782ae8ad6e78595513cce04cb42a17a5e39485
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-09 23:11:04 +0300

    filtering products condition taken into account for proper pagination display in reducer functions in pagination-slice.js file

--- src/store/slices/pagination-slice.js
+++ src/store/slices/pagination-slice.js
@@ -11,18 +11,32 @@ const paginationSlice = createSlice({
   initialState,
   reducers: {
     setTotalPageNumbers(state, action) {
-      state.pageNumbers = [
-        1,
-        2,
-        3,
-        4,
-        "...",
-        action.payload - 3,
-        action.payload - 2,
-        action.payload - 1,
-        action.payload,
-      ];
-      state.totalNumberOfPages = action.payload;
+      const { totalNumberOfPages } = action.payload;
+      state.totalNumberOfPages = totalNumberOfPages;
+      // if there is no filtering of products, all 55 pages will be displayed.
+      // in case of any filtering applied, if "state.totalNumberOfPages" will
+      // again be larger than 8, the following logic will still be applied for
+      // populating "state.pageNumbers".
+      if (state.totalNumberOfPages > 8) {
+        state.pageNumbers = [
+          1,
+          2,
+          3,
+          4,
+          "...",
+          totalNumberOfPages - 3,
+          totalNumberOfPages - 2,
+          totalNumberOfPages - 1,
+          totalNumberOfPages,
+        ];
+      } else {
+        // if filtering of products is in action though, state.pageNumbers = [1, 2, ..., state.totalNumberOfPages]
+        // only if current "state.totalNumberOfPages" is less than 8 so that above assignment cannot be held.
+        // so, update "state.pageNumbers" as required.
+        state.pageNumbers = [
+          ...Array.from({ length: state.totalNumberOfPages }, (_, i) => i + 1),
+        ];
+      }
     },
     incrementPageNumber(state) {
       // if the last page is reached, do not further increment the page number and immediately return
@@ -82,6 +96,10 @@ const paginationSlice = createSlice({
       let updatedPageNumbers = [...state.pageNumbers]; // get, current "pageNumbers" collection
       /**
        * There will be 3 distinct cases which should be evaluated for decrementing page number.
+       * Before going further on explanation, note that those 3 distinct cases can only be in
+       * action for the cases that either there is no filtering action of products by a criterion
+       * in place or after filtering is applied, "state.totalNumberOfPages" is still greater than 8.
+       * --------------------------------------------------------------------------------------------
        * The first one is that decrementing occurs for the page number less than 5. In this case,
        * no matter what the current page number is, the first 4 page numbers at the left of pagination
        * should be 1, 2, 3 and 4 in given order. The last 4 page numbers should be as expected.
@@ -97,42 +115,44 @@ const paginationSlice = createSlice({
        * order from left to right.
        */
 
-      if (state.pageNumber < 5) {
-        updatedPageNumbers = [
-          1,
-          2,
-          3,
-          4,
-          "...",
-          state.totalNumberOfPages - 3,
-          state.totalNumberOfPages - 2,
-          state.totalNumberOfPages - 1,
-          state.totalNumberOfPages,
-        ];
-      } else if (state.pageNumber >= 5) {
-        if (state.pageNumber < state.totalNumberOfPages - 4) {
+      if (state.totalNumberOfPages > 8) {
+        if (state.pageNumber < 5) {
           updatedPageNumbers = [
-            state.pageNumbers[0] - 1,
-            state.pageNumbers[0],
-            state.pageNumbers[0] + 1,
-            state.pageNumbers[0] + 2,
+            1,
+            2,
+            3,
+            4,
             "...",
             state.totalNumberOfPages - 3,
             state.totalNumberOfPages - 2,
             state.totalNumberOfPages - 1,
             state.totalNumberOfPages,
           ];
-        } else {
-          updatedPageNumbers = [
-            state.totalNumberOfPages - 7,
-            state.totalNumberOfPages - 6,
-            state.totalNumberOfPages - 5,
-            state.totalNumberOfPages - 4,
-            state.totalNumberOfPages - 3,
-            state.totalNumberOfPages - 2,
-            state.totalNumberOfPages - 1,
-            state.totalNumberOfPages,
-          ];
+        } else if (state.pageNumber >= 5) {
+          if (state.pageNumber < state.totalNumberOfPages - 4) {
+            updatedPageNumbers = [
+              state.pageNumbers[0] - 1,
+              state.pageNumbers[0],
+              state.pageNumbers[0] + 1,
+              state.pageNumbers[0] + 2,
+              "...",
+              state.totalNumberOfPages - 3,
+              state.totalNumberOfPages - 2,
+              state.totalNumberOfPages - 1,
+              state.totalNumberOfPages,
+            ];
+          } else {
+            updatedPageNumbers = [
+              state.totalNumberOfPages - 7,
+              state.totalNumberOfPages - 6,
+              state.totalNumberOfPages - 5,
+              state.totalNumberOfPages - 4,
+              state.totalNumberOfPages - 3,
+              state.totalNumberOfPages - 2,
+              state.totalNumberOfPages - 1,
+              state.totalNumberOfPages,
+            ];
+          }
         }
       }
       state.pageNumbers = updatedPageNumbers;
