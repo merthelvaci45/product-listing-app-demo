0acc3e4dd8977bbf9fc2d6005f97cac4eadaaac9
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-09 06:45:25 +0300

    "paginationReducer" and "paginationActions" re-exported in /src/store/slices/index.js file

--- src/store/slices/index.js
+++ src/store/slices/index.js
@@ -1,4 +1,12 @@
 import { cartReducer, cartActions } from "./cart-slice";
+import { paginationReducer, paginationActions } from "./pagination-slice";
 import { productsReducer, productsActions } from "./products-slice";
 
-export { cartReducer, cartActions, productsReducer, productsActions };
+export {
+  cartReducer,
+  cartActions,
+  paginationReducer,
+  paginationActions,
+  productsReducer,
+  productsActions,
+};
