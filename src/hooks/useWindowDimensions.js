fecbb1b0ce4c02b0e257de992559908b61f9c5b0
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-10 14:58:30 +0300

    useWindowDimensions hook created in useWindowDimensions.js file

---
+++ src/hooks/useWindowDimensions.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+/**
+ * this helper function is used to obtain current browser width and height
+ */
+const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+};
+
+/**
+ * this custom hook is used to obtain current window width of browser when needed
+ */
+const useWindowDimensions = () => {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions(getWindowDimensions());
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+};
+
+export default useWindowDimensions;
