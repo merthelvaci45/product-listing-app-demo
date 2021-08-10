659d5de375937adc3eef72f65a22ab85eae0ebda
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-10 10:34:16 +0300

    actual logo image rendered in Logo.jsx file

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,7 +1,19 @@
 import classes from "./Logo.module.scss";
 
+import { logoSvg } from "./util";
+
+import Image from "../Image";
+
 const Logo = () => {
-  return <div className={classes.Logo}>Logo</div>;
+  return (
+    <div className={classes.Logo}>
+      <Image
+        imgAltText={logoSvg.altText}
+        imgFallbackSrc={logoSvg.source}
+        isSvg
+      />
+    </div>
+  );
 };
 
 export default Logo;
