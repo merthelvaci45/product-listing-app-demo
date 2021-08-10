8a11dd01943457a1d128a7338d52ba7fa3772f78
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-10 17:56:08 +0300

    Footer component returned as React Portal in Footer.jsx file

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,15 +1,20 @@
+import { createPortal } from "react-dom";
+
 import classes from "./Footer.module.scss";
 
 import { Text } from "..";
 
 const Footer = () => {
-  return (
+  const footerNode = document.querySelector("#footer");
+  const footerContent = (
     <footer className={classes.Footer}>
       <Text fontSize="FontSize13" text="©2019 Market" />
       <Text fontSize="FontSize13" text="•" />
       <Text fontSize="FontSize13" text="Privacy Policy" />
     </footer>
   );
+
+  return createPortal(footerContent, footerNode);
 };
 
 export default Footer;
