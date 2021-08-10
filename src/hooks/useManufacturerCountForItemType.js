153cc68a85feced36be6c5be5a179ffd9b755068
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-10 19:07:03 +0300

    logic and content of "useTagCountForItemType" and "useManufacturerCountForItemType" hooks changed in useTagCountForItemType.js and useManufacturerCountForItemType.js files, respectively

--- src/hooks/useManufacturerCountForItemType.js
+++ src/hooks/useManufacturerCountForItemType.js
@@ -4,39 +4,50 @@ import { findNumberOfOccurencesOfItemsInArray } from "../utils";
 
 /**
  * this hook is responsible for extracting an object, whose keys are made out of each manufacturer name
- * and corresponding values are the count of how many products each manufacturer has for "itemType" argument.
+ * and corresponding values are the count of how many products each manufacturer has for "mug" and "shirt" item types.
  * @param {Array} items: array of all products/items to be filtered out
- * @param {String} itemType: "mug" || "shirt"
  */
-const useManufacturerCountForItemType = (items = [], itemType = "") => {
-  const [manufacturerCountForItemType, setManufacturerCountForItemType] =
-    useState();
+const useManufacturerCountForItemType = (items = []) => {
+  const [manufacturerCountForMug, setManufacturerCountForMug] = useState();
+
+  const [manufacturerCountForShirt, setManufacturerCountForShirt] = useState();
 
   useEffect(() => {
     // run this hook only if "items" has the populated data
     if (items.length > 0) {
-      // first, extract all items with specific "itemType"
-      const itemsOfSpecificType = items.filter(
-        (item) => item.itemType === itemType
+      // first, extract all items with specific "mug" and "shirt" types
+      const itemsOfMugType = items.filter((item) => item.itemType === "mug");
+      const itemsOfShirtType = items.filter(
+        (item) => item.itemType === "shirt"
       );
 
-      // second, extract all brands available for that specific "itemType"
-      const availableBrandsForItemType = itemsOfSpecificType.map(
+      // second, extract all brands available for "mug" and "shirt" types
+      const availableBrandsForMugType = itemsOfMugType.map(
         (itemType) => itemType.manufacturer
       );
 
-      // third, find out how many items with specific "itemType" a manufacturer has
-      // and "manufacturerCountForItemType" state accordingly.
+      const availableBrandsForShirtType = itemsOfShirtType.map(
+        (itemType) => itemType.manufacturer
+      );
+
+      // third, find out how many items with specific "itemType", i.e, "mug" or "shirt", a manufacturer has
+      // and update "manufacturerCountForMugType" and "manufacturerCountForShirtType" states accordingly.
       // for this purpose, use "findNumberOfOccurencesOfItemsInArray" utility function
       // defined in "../utils/findNumberOfOccurencesOfItemsInArray.js" file
-      setManufacturerCountForItemType(() => ({
+      setManufacturerCountForMug(() => ({
         //All: items.length,
         "Brands - All": items.length,
-        ...findNumberOfOccurencesOfItemsInArray(availableBrandsForItemType),
+        ...findNumberOfOccurencesOfItemsInArray(availableBrandsForMugType),
+      }));
+
+      setManufacturerCountForShirt(() => ({
+        //All: items.length,
+        "Brands - All": items.length,
+        ...findNumberOfOccurencesOfItemsInArray(availableBrandsForShirtType),
       }));
     }
-  }, [items]); // eslint-disable-line react-hooks/exhaustive-deps
-  return manufacturerCountForItemType;
+  }, [items]);
+  return [manufacturerCountForMug, manufacturerCountForShirt];
 };
 
 export default useManufacturerCountForItemType;
--- src/hooks/useTagCountForItemType.js
+++ src/hooks/useTagCountForItemType.js
@@ -4,39 +4,53 @@ import { findNumberOfOccurencesOfItemsInArray } from "../utils";
 
 /**
  * this hook is responsible for extracting an object, whose keys are made out of each tag name
- * and corresponding values are the count of how many products with each tag name for "itemType" argument.
+ * and corresponding values are the count of how many products with each tag name for "mug" amd "shirt" item types.
  * @param {Array} items: array of all products/items to be filtered out
- * @param {String} itemType: "mug" || "shirt"
  */
-const useTagCountForItemType = (items = [], itemType = "") => {
-  const [tagCountForItemType, setTagCountForItemType] = useState();
+const useTagCountForItemType = (items = []) => {
+  const [tagCountForMug, setTagCountForMug] = useState();
+  const [tagCountForShirt, setTagCountForShirt] = useState();
 
   useEffect(() => {
     // run this hook only if "items" has the populated data
     if (items.length > 0) {
       // first, extract all items with specific "itemType"
-      const itemsOfSpecificType = items.filter(
-        (item) => item.itemType === itemType
+      const itemsOfMugType = items.filter((item) => item.itemType === "mug");
+
+      const itemsOfShirtType = items.filter(
+        (item) => item.itemType === "shirt"
       );
 
       // second, extract all tags available for that specific "itemType"
-      const availableTagsForItemType = itemsOfSpecificType.reduce(
+      const availableTagsForMugType = itemsOfMugType.reduce(
         (acc, cur) => [...acc, ...cur.tags],
         []
       );
 
-      // third, find out how many items with specific "itemType" a tag has
-      // and "tagCountForItemType" state accordingly.
+      const availableTagsForShirtType = itemsOfShirtType.reduce(
+        (acc, cur) => [...acc, ...cur.tags],
+        []
+      );
+
+      // third, find out how many items with "mug" type, a tag has
+      // and update "tagCountForItemType" state accordingly.
       // for this purpose, use "findNumberOfOccurencesOfItemsInArray" utility function
       // defined in "../utils/findNumberOfOccurencesOfItemsInArray.js" file
-      setTagCountForItemType(() => ({
+      setTagCountForMug(() => ({
         //All: items.length,
         "Tags - All": items.length,
-        ...findNumberOfOccurencesOfItemsInArray(availableTagsForItemType),
+        ...findNumberOfOccurencesOfItemsInArray(availableTagsForMugType),
+      }));
+
+      setTagCountForShirt(() => ({
+        //All: items.length,
+        "Tags - All": items.length,
+        ...findNumberOfOccurencesOfItemsInArray(availableTagsForShirtType),
       }));
     }
   }, [items]); // eslint-disable-line react-hooks/exhaustive-deps
-  return tagCountForItemType;
+
+  return [tagCountForMug, tagCountForShirt];
 };
 
 export default useTagCountForItemType;
