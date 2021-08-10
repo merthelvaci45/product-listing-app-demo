efe5c497ccd08fa484bcbbac7ce0000e20b8954c
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-10 10:35:37 +0300

    dummy product image exported as object in /src/components/ProductCard/util.js file

---
+++ src/components/ProductCard/util.js
@@ -0,0 +1,10 @@
+import dummyProductFallbackImage from "../../static/assets/products/product.jpeg";
+import dummyProductWebpImage from "../../static/assets/products/webp/product.webp";
+
+export const dummyProductImage = {
+  source: {
+    fallback: dummyProductFallbackImage,
+    webp: dummyProductWebpImage,
+  },
+  altText: "Dummy product image displayed for each product",
+};
