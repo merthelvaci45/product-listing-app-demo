fd944a45ed2b9b3cefdf1cfe49dd8ce6fd10c646
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-10 17:47:33 +0300

    all components re-exported in /src/components/index.js file

---
+++ src/components/index.js
@@ -0,0 +1,47 @@
+import Backdrop from "./Backdrop";
+import Button from "./Button";
+import Checkbox from "./Checkbox";
+import FeatureCardWithTitle from "./FeatureCardWithTitle";
+import FlatButton from "./FlatButton";
+import Footer from "./Footer";
+import Header from "./Header";
+import Image from "./Image";
+import Input from "./Input";
+import ItemType from "./ItemType";
+import Layout from "./Layout";
+import Logo from "./Logo";
+import Modal from "./Modal";
+import Pagination from "./Pagination";
+import ProductCard from "./ProductCard";
+import ProductInShoppingCart from "./ProductInShoppingCart";
+import RadioButton from "./RadioButton";
+import ShoppingCart from "./ShoppingCart";
+import SortingAndFilteringSection from "./SortingAndFilteringSection";
+import Spinner from "./Spinner";
+import Text from "./Text";
+import Title from "./Title";
+
+export {
+  Backdrop,
+  Button,
+  Checkbox,
+  FeatureCardWithTitle,
+  FlatButton,
+  Footer,
+  Header,
+  Image,
+  Input,
+  ItemType,
+  Layout,
+  Logo,
+  Modal,
+  Pagination,
+  ProductCard,
+  ProductInShoppingCart,
+  RadioButton,
+  ShoppingCart,
+  SortingAndFilteringSection,
+  Spinner,
+  Text,
+  Title,
+};
