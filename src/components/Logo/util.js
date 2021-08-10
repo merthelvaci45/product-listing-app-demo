ffd4bde1ba5c541c5908193ec9179468a7c8d78a
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-10 10:34:02 +0300

    logoSvgImage exported as object in /src/components/Logo/util.js file

---
+++ src/components/Logo/util.js
@@ -0,0 +1,6 @@
+import logoSvgImage from "../../static/assets/svg/logo/logo.svg";
+
+export const logoSvg = {
+  altText: "Dummy market logo in svg format and white color",
+  source: logoSvgImage,
+};
