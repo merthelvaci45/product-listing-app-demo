00cae97429c8ad069784d53353030ce015114a2d
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-10 14:58:47 +0300

    useWindowDimensions hook re-exported in /src/hooks/index.js file

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,5 +1,11 @@
 import useAPI from "./useAPI";
 import useManufacturerCountForItemType from "./useManufacturerCountForItemType";
 import useTagCountForItemType from "./useTagCountForItemType";
+import useWindowDimensions from "./useWindowDimensions";
 
-export { useAPI, useManufacturerCountForItemType, useTagCountForItemType };
+export {
+  useAPI,
+  useManufacturerCountForItemType,
+  useTagCountForItemType,
+  useWindowDimensions,
+};
