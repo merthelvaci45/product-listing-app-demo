ac602febe6701c0585c6e5f7167da734395bb5c4
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-12 02:32:32 +0300

    product(s) filtering logic improved for Brand and/or Tag selection in useManufacturerCountForItemType.js and useTagCountForItemType.js files (STILL, NOT IN DESIRED FINAL SHAPE; THOUGH!)

--- src/hooks/useManufacturerCountForItemType.js
+++ src/hooks/useManufacturerCountForItemType.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 import { findNumberOfOccurencesOfItemsInArray } from "../utils";
 
@@ -9,44 +10,93 @@ import { findNumberOfOccurencesOfItemsInArray } from "../utils";
  */
 const useManufacturerCountForItemType = (items = []) => {
   const [manufacturerCountForMug, setManufacturerCountForMug] = useState();
-
   const [manufacturerCountForShirt, setManufacturerCountForShirt] = useState();
+
+  const { appliedTagFilters } = useSelector((state) => state.productsSlice);
 
   useEffect(() => {
     // run this hook only if "items" has the populated data
     if (items.length > 0) {
       // first, extract all items with specific "mug" and "shirt" types
       const itemsOfMugType = items.filter((item) => item.itemType === "mug");
-      const itemsOfShirtType = items.filter(
-        (item) => item.itemType === "shirt"
-      );
+      const itemsOfShirtType = items.filter((item) => item.itemType === "shirt");
 
       // second, extract all brands available for "mug" and "shirt" types
-      const availableBrandsForMugType = itemsOfMugType.map(
-        (itemType) => itemType.manufacturer
-      );
-
-      const availableBrandsForShirtType = itemsOfShirtType.map(
-        (itemType) => itemType.manufacturer
-      );
+      const availableBrandsForMugType = itemsOfMugType.map((itemType) => itemType.manufacturer);
+      const availableBrandsForShirtType = itemsOfShirtType.map((itemType) => itemType.manufacturer);
 
       // third, find out how many items with specific "itemType", i.e, "mug" or "shirt", a manufacturer has
       // and update "manufacturerCountForMugType" and "manufacturerCountForShirtType" states accordingly.
       // for this purpose, use "findNumberOfOccurencesOfItemsInArray" utility function
       // defined in "../utils/findNumberOfOccurencesOfItemsInArray.js" file
-      setManufacturerCountForMug(() => ({
-        //All: items.length,
-        "Brands - All": items.length,
-        ...findNumberOfOccurencesOfItemsInArray(availableBrandsForMugType),
-      }));
+      setManufacturerCountForMug(() => {
+        // if 'Tags - All' checkbox is NOT selected,
+        if (!appliedTagFilters[0]?.includes("All")) {
+          // and if no specific tag checkbox is selected,
+          if (appliedTagFilters?.length === 0) {
+            return availableBrandsForMugType.reduce((brandCounts, brand) => {
+              return {
+                ...brandCounts,
+                "Brands - All": 0,
+                [brand]: 0,
+              };
+            }, {});
+          }
+          // if one or more tag checkbox is/are selected,
+          return availableBrandsForMugType.reduce((brandCounts, brand) => {
+            return {
+              ...brandCounts,
+              "Brands - All": items.filter((prod) => prod.tags.some((tag) => appliedTagFilters?.includes(tag))).length,
+              [brand]: items
+                .filter((prod) => prod.itemType === "mug")
+                .filter((prod) => prod.tags.some((tag) => appliedTagFilters?.includes(tag)))
+                .filter((prod) => prod.manufacturer === brand).length,
+            };
+          }, {});
+        }
 
-      setManufacturerCountForShirt(() => ({
-        //All: items.length,
-        "Brands - All": items.length,
-        ...findNumberOfOccurencesOfItemsInArray(availableBrandsForShirtType),
-      }));
+        // if 'Tags - All' checkbox is selected,
+        return {
+          "Brands - All": items.length,
+          ...findNumberOfOccurencesOfItemsInArray(availableBrandsForMugType),
+        };
+      });
+
+      setManufacturerCountForShirt(() => {
+        // if 'Tags - All' checkbox is NOT selected,
+        if (!appliedTagFilters[0]?.includes("All")) {
+          // and if no specific tag checkbox is selected,
+          if (appliedTagFilters?.length === 0) {
+            return availableBrandsForShirtType.reduce((brandCounts, brand) => {
+              return {
+                ...brandCounts,
+                "Brands - All": 0,
+                [brand]: 0,
+              };
+            }, {});
+          }
+          // if one or more tag checkbox is/are selected,
+          return availableBrandsForShirtType.reduce((brandCounts, brand) => {
+            return {
+              ...brandCounts,
+              "Brands - All": items.filter((prod) => prod.tags.some((tag) => appliedTagFilters?.includes(tag))).length,
+              [brand]: items
+                .filter((prod) => prod.itemType === "shirt")
+                .filter((prod) => prod.tags.some((tag) => appliedTagFilters?.includes(tag)))
+                .filter((prod) => prod.manufacturer === brand).length,
+            };
+          }, {});
+        }
+
+        // if 'Tags - All' checkbox is selected,
+        return {
+          "Brands - All": items.length,
+          ...findNumberOfOccurencesOfItemsInArray(availableBrandsForShirtType),
+        };
+      });
     }
-  }, [items]);
+  }, [items, appliedTagFilters]);
+
   return [manufacturerCountForMug, manufacturerCountForShirt];
 };
 
--- src/hooks/useTagCountForItemType.js
+++ src/hooks/useTagCountForItemType.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 
 import { findNumberOfOccurencesOfItemsInArray } from "../utils";
 
@@ -11,44 +12,90 @@ const useTagCountForItemType = (items = []) => {
   const [tagCountForMug, setTagCountForMug] = useState();
   const [tagCountForShirt, setTagCountForShirt] = useState();
 
+  const { appliedBrandFilters } = useSelector((state) => state.productsSlice);
+
   useEffect(() => {
     // run this hook only if "items" has the populated data
     if (items.length > 0) {
       // first, extract all items with specific "itemType"
       const itemsOfMugType = items.filter((item) => item.itemType === "mug");
-
-      const itemsOfShirtType = items.filter(
-        (item) => item.itemType === "shirt"
-      );
+      const itemsOfShirtType = items.filter((item) => item.itemType === "shirt");
 
       // second, extract all tags available for that specific "itemType"
-      const availableTagsForMugType = itemsOfMugType.reduce(
-        (acc, cur) => [...acc, ...cur.tags],
-        []
-      );
-
-      const availableTagsForShirtType = itemsOfShirtType.reduce(
-        (acc, cur) => [...acc, ...cur.tags],
-        []
-      );
+      const availableTagsForMugType = itemsOfMugType.reduce((acc, cur) => [...acc, ...cur.tags], []);
+      const availableTagsForShirtType = itemsOfShirtType.reduce((acc, cur) => [...acc, ...cur.tags], []);
 
       // third, find out how many items with "mug" type, a tag has
       // and update "tagCountForItemType" state accordingly.
       // for this purpose, use "findNumberOfOccurencesOfItemsInArray" utility function
       // defined in "../utils/findNumberOfOccurencesOfItemsInArray.js" file
-      setTagCountForMug(() => ({
-        //All: items.length,
-        "Tags - All": items.length,
-        ...findNumberOfOccurencesOfItemsInArray(availableTagsForMugType),
-      }));
+      setTagCountForMug(() => {
+        // if 'Brands - All' checkbox is NOT selected,
+        if (!appliedBrandFilters[0]?.includes("All")) {
+          // and if no specific tag checkbox is selected,
+          if (appliedBrandFilters?.length === 0) {
+            return availableTagsForMugType.reduce((tagCounts, tag) => {
+              return {
+                ...tagCounts,
+                "Tags - All": 0,
+                [tag]: 0,
+              };
+            }, {});
+          }
+          // if one or more brand checkbox is/are selected,
+          return availableTagsForMugType.reduce((tagCounts, tag) => {
+            return {
+              ...tagCounts,
+              "Tags - All": items.filter((prod) => appliedBrandFilters.includes(prod.manufacturer)).length,
+              [tag]: items
+                .filter((prod) => prod.itemType === "mug")
+                .filter((prod) => appliedBrandFilters.includes(prod.manufacturer))
+                .filter((prod) => prod.tags.includes(tag)).length,
+            };
+          }, {});
+        }
 
-      setTagCountForShirt(() => ({
-        //All: items.length,
-        "Tags - All": items.length,
-        ...findNumberOfOccurencesOfItemsInArray(availableTagsForShirtType),
-      }));
+        // if 'Brands - All' checkbox is selected,
+        return {
+          "Tags - All": items.length,
+          ...findNumberOfOccurencesOfItemsInArray(availableTagsForMugType),
+        };
+      });
+
+      setTagCountForShirt(() => {
+        // if 'Brands - All' checkbox is NOT selected,
+        if (!appliedBrandFilters[0]?.includes("All")) {
+          // and if no specific tag checkbox is selected,
+          if (appliedBrandFilters?.length === 0) {
+            return availableTagsForShirtType.reduce((tagCounts, tag) => {
+              return {
+                ...tagCounts,
+                "Tags - All": 0,
+                [tag]: 0,
+              };
+            }, {});
+          }
+          // if one or more brand checkbox is/are selected,
+          return availableTagsForShirtType.reduce((tagCounts, tag) => {
+            return {
+              ...tagCounts,
+              "Tags - All": items.filter((prod) => appliedBrandFilters.includes(prod.manufacturer)).length,
+              [tag]: items
+                .filter((prod) => prod.itemType === "mug")
+                .filter((prod) => appliedBrandFilters.includes(prod.manufacturer))
+                .filter((prod) => prod.tags.includes(tag)).length,
+            };
+          }, {});
+        }
+
+        // if 'Brands - All' checkbox is selected,
+        return {
+          "Tags - All": items.length,
+          ...findNumberOfOccurencesOfItemsInArray(availableTagsForShirtType),
+        };
+      });
     }
-  }, [items]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [items, appliedBrandFilters]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return [tagCountForMug, tagCountForShirt];
 };
