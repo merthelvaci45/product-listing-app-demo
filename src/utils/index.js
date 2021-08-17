2347daafa200ace65832b732da8c0d891819e998
merthelvaci <mert.helvaci@siimdatix.com>
2021-08-17 23:07:39 +0300

    import and re-export of "COMPANIES_API_BASE_URL" constant removed from /src/utils/index.js file

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,17 +1,9 @@
-import {
-  COMPANIES_API_BASE_URL,
-  ITEMS_API_BASE_URL,
-  API_METHODS,
-  PAGE_ROUTES,
-  SORT_OPTIONS,
-  SORT_OPTIONS_IDS,
-} from "./constants";
+import { ITEMS_API_BASE_URL, API_METHODS, PAGE_ROUTES, SORT_OPTIONS, SORT_OPTIONS_IDS } from "./constants";
 import findNumberOfOccurencesOfItemsInArray from "./findNumberOfOccurencesOfItemsInArray";
 import initialSortingRadioButtonsState from "./localInitialStates";
 
 export {
   API_METHODS,
-  COMPANIES_API_BASE_URL,
   ITEMS_API_BASE_URL,
   PAGE_ROUTES,
   SORT_OPTIONS,
